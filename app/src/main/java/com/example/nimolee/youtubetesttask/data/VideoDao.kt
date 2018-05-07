63de2724bed3d8d728d87dadb944cd775e968226
Serhiy Kovernik <Serhiy-120397@ukr.net>
2018-05-07 10:09:04 +0300

    Add youtube dependencies, internet permission. Write entity and dao for store video info.

---
+++ app/src/main/java/com/example/nimolee/youtubetesttask/data/VideoDao.kt
@@ -0,0 +1,28 @@
+package com.example.nimolee.youtubetesttask.data
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Delete
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy.REPLACE
+import android.arch.persistence.room.Query
+
+@Dao
+interface VideoDao {
+    @Query("select * from video")
+    fun getAllVideo(): Array<VideoEntity>?
+
+    @Query("select * from video where id = :videoId")
+    fun getVideo(videoId: Int): VideoEntity?
+
+    @Insert(onConflict = REPLACE)
+    fun insertVideo(videoEntity: VideoEntity)
+
+    @Delete
+    fun removeVideo(videoEntity: VideoEntity)
+
+    @Delete
+    fun removeVideos(videos: ArrayList<VideoEntity>): Int
+
+    @Query("delete from video")
+    fun clearTable()
+}
---
+++ app/src/main/java/com/example/nimolee/youtubetesttask/data/VideoEntity.kt
@@ -0,0 +1,12 @@
+package com.example.nimolee.youtubetesttask.data
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+import android.media.Image
+
+@Entity(tableName = "video")
+data class VideoEntity(@PrimaryKey(autoGenerate = true) val id: Int,
+                       @ColumnInfo(name = "video_name") val name: String,
+                       @ColumnInfo(name = "video_description") val description: String,
+                       @ColumnInfo(name = "video_thumbnail") val thumbnail: Image)
